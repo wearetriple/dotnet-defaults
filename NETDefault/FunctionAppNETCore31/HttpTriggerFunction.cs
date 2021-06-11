c05ec2bded4ae1644e29cacd8929bce984dd1bb1
Thomas Bleijendaal <t.bleijendaal@wearetriple.com>
2021-06-11 11:33:17 +0200

    PR fix

--- NETDefault/FunctionAppNETCore31/HttpTriggerFunction.cs
+++ NETDefault/FunctionAppNETCore31/HttpTriggerFunction.cs
@@ -1,14 +1,13 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Threading.Tasks;
+using FunctionAppNETCore31.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 using Microsoft.Extensions.Options;
-using FunctionAppNETCore31.Models;
+using Newtonsoft.Json;
 
 namespace FunctionAppNETCore31
 {
