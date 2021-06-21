aff4e09a879fe7ed6e6a4b9f9de88c464a14f261
Thomas Bleijendaal <ThomasBleijendaal@users.noreply.github.com>
2021-06-21 15:56:27 +0200

    Health check + Application warm-up + Slot swap warm-up (#19)

---
+++ NETDefault/WebApp/Models/HttpPathSettings.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+
+namespace WebApp.Models
+{
+    public class HttpPathSettings
+    {
+        public IEnumerable<PathString> Paths { get; set; }
+    }
+}
