096a60822c3e4342e63ea555ed79bfcc3606b484
Dennis Jakupovic <24221025+Nuss9@users.noreply.github.com>
2021-11-23 12:19:32 +0100

    Add requestHandler sample

---
+++ NETDefault/RequestHandlerNETCore31/HelperClasses/HttpRequestExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace RequestHandlerNETCore31.HelperClasses
+{
+    internal static class HttpRequestExtensions
+    {
+        public static async Task<string> ReadAsStringAsync(this HttpRequest request)
+        {
+            request.EnableBuffering();
+            string result = null;
+            using (var reader = new StreamReader(request.Body, Encoding.UTF8, detectEncodingFromByteOrderMarks: true, 1024, leaveOpen: true))
+            {
+                result = await reader.ReadToEndAsync();
+            }
+
+            request.Body.Seek(0L, SeekOrigin.Begin);
+            return result;
+        }
+    }
+}
