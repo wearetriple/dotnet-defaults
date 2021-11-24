258cc5eedd21ac83f1b7f2940fa5fe723e4d7e17
Dennis Jakupovic <24221025+Nuss9@users.noreply.github.com>
2021-11-24 08:59:09 +0100

    Add comment on Azure dependency

--- NETDefault/RequestHandlerNETCore31/HelperClasses/HttpRequestExtensions.cs
+++ NETDefault/RequestHandlerNETCore31/HelperClasses/HttpRequestExtensions.cs
@@ -7,10 +7,12 @@ namespace RequestHandlerNETCore31.HelperClasses
 {
     internal static class HttpRequestExtensions
     {
+        // When making use of Azure Functions, the dependency on Microsoft.Azure.WebJobs.Extensions.Http is adviced, since it contains more useful operations.
+        // Calling this function can then be done via the dependency instead.
         public static async Task<string> ReadAsStringAsync(this HttpRequest request)
         {
             request.EnableBuffering();
-            string result = null;
+            string? result = null;
             using (var reader = new StreamReader(request.Body, Encoding.UTF8, detectEncodingFromByteOrderMarks: true, 1024, leaveOpen: true))
             {
                 result = await reader.ReadToEndAsync();
