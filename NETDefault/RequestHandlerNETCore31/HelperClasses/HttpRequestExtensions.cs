a9747614f1736904ee14db96d3253d33dcb382af
Nuss9 <24221025+Nuss9@users.noreply.github.com>
2022-02-18 16:32:14 +0100

    Issue #43 - RequestHandlers (#49)

---
+++ NETDefault/RequestHandlerNETCore31/HelperClasses/HttpRequestExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace RequestHandlerNETCore31.HelperClasses
+{
+    internal static class HttpRequestExtensions
+    {
+        // When making use of Azure Functions, the dependency on Microsoft.Azure.WebJobs.Extensions.Http is adviced, since it contains more useful operations.
+        // Calling this function can then be done via the dependency instead.
+        public static async Task<string> ReadAsStringAsync(this HttpRequest request)
+        {
+            request.EnableBuffering();
+            string? result = null;
+            using (var reader = new StreamReader(request.Body, Encoding.UTF8, detectEncodingFromByteOrderMarks: true, 1024, leaveOpen: true))
+            {
+                result = await reader.ReadToEndAsync();
+            }
+
+            request.Body.Seek(0L, SeekOrigin.Begin);
+            return result;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -24,6 +24,8 @@ Common boilerplate, practices, and guidelines for Triple .NET projects.
 
 - [Coding guidelines](CodingGuidelines.md)
 - [Kanban guidelines](KanbanGuidelines.md)
+- [Vocabulary](Vocabulary.md)
+- [Source Control](SourceControl.md)
 
 The key words “MUST”, “MUST NOT”, “REQUIRED”, “SHALL”, “SHALL NOT”, “SHOULD”, “SHOULD NOT”, “RECOMMENDED”, “MAY”, and “OPTIONAL” in this document are to be interpreted as described in [[RFC 2119](https://www.ietf.org/rfc/rfc2119.txt)].
 
