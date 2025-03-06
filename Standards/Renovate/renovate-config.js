04e41a6643d343f5dfaea5ee1babb5a6a65e03b2
Mark Hijdra <mark.hijdra@wearetriple.com>
2025-03-06 16:07:49 +0100

    Renovate configuration update

--- Standards/Renovate/renovate-config.js
+++ Standards/Renovate/renovate-config.js
@@ -5,9 +5,9 @@
             matchHost: 'https://pkgs.dev.azure.com/{OrganisationName}'
         },
     ],
-    //settings on where to find your repo.
+    // Settings on where to find your repo.
     endpoint: "https://dev.azure.com/{OrganisationName}",
     platform: "azure",
-    token: process.env["RENOVATE_TOKEN"],// Put your renovate PAT here
+    token: process.env["RENOVATE_TOKEN"],
     repositories: ['OrganisationName/Repo.Name']
 };
